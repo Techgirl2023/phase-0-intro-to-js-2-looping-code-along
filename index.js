835b57598e793657697d005d6382fc99c5144b7c
Ashanti Logan <Ashantilogan2@gmail.com>
2023-08-21 16:29:33 -0400

    commit tree e56d2431013a2d44a94c5c1a25fa58bbf7ad0172

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"]; 
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bowl!`);
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+const cards = ["Toi", "Summer", "Liza"]
+
+function writeCards(cards) {
+    const  messagesArray = []
+    for (let i = 0; i < cards.length; i++) {
+        messagesArray.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+    return messagesArray
+}
+writeCards(cards);
+
+function countDown() {
+    let countDown = 10;
+    while (countDown > -1 ) {
+        console.log(countDown--);
+    }
+}
+
